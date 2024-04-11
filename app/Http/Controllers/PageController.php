0149d115f27680554f9e4dce5fa8b084491fc5f7
andrealivornese <andrealivornese@gmail.com>
2024-04-11 16:45:30 +0200

    lettura dei dati nella tabella del db e stampa in pagina

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,7 +12,9 @@ class PageController extends Controller
      */
     public function index()
     {
-        return view("layouts.app");
+        $comics = Comic::all();
+
+        return view("home", compact('comics'));
     }
 
     /**
